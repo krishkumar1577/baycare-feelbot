04c782c5aba0da36aa3043756d9a566898d01dc1
krish <135630974+krishkumar1577@users.noreply.github.com>
2025-09-02 19:13:25 +0000

    feat: enhance LandingPage with styling and layout

--- frontend/src/Pages/Landing_page.tsx
+++ frontend/src/Pages/Landing_page.tsx
@@ -3,8 +3,11 @@ import React from "react";
 const LandingPage: React.FC = () => {
     return (
         <main style={{ minHeight: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-            <h1>Your Personal AI Healcare Companion</h1>
-            
+            <h1 className="text-4xl font-bold mb-8">Your Personal AI Healcare Companion</h1>
+            <div className="grid grid-cols-2 gap-4 ">
+                <div className="bg-white p-4 w-64 h-64 border-4 border-pink-500 rounded-lg shadow-md ">Baycare</div>
+                <div className="bg-white p-4 w-64 h-64 border-4 border-pink-500 rounded-lg shadow-md">Feelbot</div>
+            </div>
             
         </main>
     );
