440a65d3ca659aa845502bd7cd14b1f5c91fc894
krish <135630974+krishkumar1577@users.noreply.github.com>
2025-09-02 18:55:06 +0000

    feat: initialize frontend with React, Vite, and Tailwind CSS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import LandingPage from './Pages/Landing_page.tsx'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <LandingPage />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Pages/Landing_page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LandingPage: React.FC = () => {
+    return (
+        <main style={{ minHeight: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+            <h1>Your Personal AI Healcare Companion</h1>
+            
+            
+        </main>
+    );
+};
+
+export default LandingPage;
